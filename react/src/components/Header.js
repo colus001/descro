91284b481e6fe27f6f56ac1be9199d724f0f9c78
Seokjun Kim <seokjun.kim@streami.co>
2018-08-17 20:24:18 +0900

    Fix conflict

--- react/src/components/Header.js
+++ react/src/components/Header.js
@@ -1,5 +1,5 @@
-import React, { Fragment, Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, {Fragment, Component} from 'react'
+import {Link} from 'react-router-dom'
 
 import Modal from './Modal'
 import Balance from './Balance'
@@ -9,90 +9,97 @@ import history from '../history'
 import './Header.css'
 
 class Header extends Component {
-	state = {
+  state = {
     isShow: false,
     searchAddress: '',
-		startAddress: '',
-	}
+    startAddress: ''
+  }
 
-	componentDidMount(){
+  componentDidMount() {
     // console.log(this.props);
     // this.props.contract.getBalanceByEscrowId.call(0, {
     //   from: fromAccount,
     //   gas: gasLimit,
     //   gasPrice: gasPriceInWei
     // });
-	}
+  }
 
-	handleModal = (show) => () => {
-		const {address} = this.state
+  handleModal = (show) => () => {
+    const {startAddress} = this.state;
+    this.setState({
+      isShow: show,
+      ...show
+        ? {}
+        : {
+          startAddress: ''
+        }
+    })
+  }
 
-		this.setState({
-			isShow: show,
-			...show ? {} : {address: ''}
-		})
-	}
-
-	handleChangeInput = (name) => (e) => {
-		this.setState({
-			[name]: e.target.value,
-		})
+  handleChangeInput = (name) => (e) => {
+    this.setState({[name]: e.target.value})
   }
 
   createEscrow = () => {
     console.log(this.props);
   }
 
-	handleLogout = () => {
-		this.props.removeWalletAddress()
-		history.push('/')
-	}
+  handleLogout = () => {
+    this.props.removeWalletAddress()
+    history.push('/')
+  }
 
-	render() {
-		const { isShow, address } = this.state
+  render() {
+    const {isShow, startAddress, searchAddress} = this.state;
 
-		return (
+    return (
 			<Fragment>
-				<div className="header" key="header">
-					<div className='container'>
-						<div className="header--top">
-							<Link to="/" className="header--logo">DESCRO</Link>
-							<div className="header--search">
-								<input type="text" className='header--search-input' placeholder="Search for a deal" />
-							</div>
-							{this.props.address && (
-								<div className="header--address">
-									<button>My Account</button>
-									<button onClick={this.handleLogout}>Logout</button>
-								</div>
-							)}
-						</div>
-						<div className="header--bottom">
-							<Balance balance={12} />
-							<div className="header--action">
-								<div className="header--intro">
-									<div className="intro-title">DESCRO</div>
-									<div className="intro-body">Decentralized Escrow for Ethereum</div>
-								</div>
-								<button className="btn" onClick={this.handleModal(true)}>Start Deal</button>
-							</div>
-						</div>
-					</div>
-				</div>
-				<Modal key='modal' isShow={isShow} hideModal={this.handleModal(false)}>
-					Start Deal Modal
-					<div>
-						<label htmlFor="buyerAddress">Seller Address</label>
-						<input type='text' id="buyerAddress" value={address} onChange={this.handleChangeInput('address')} />
-					</div>
-						<div>
-							<button className="btn" onClick={this.handleModal(false)}>Cancel</button>
-							<button className="btn" onClick={this.handleModal(false)}>Confirm</button>
-						</div>
-				</Modal>
-				</Fragment>
-			)
-		}
-	}
+	      <div className="header" key="header">
+	        <div className='container'>
+	          <div className="header--top">
+	            <Link to="/" className="header--logo">DESCRO</Link>
+	            <div className="header--search">
+	              <input type="text" className='header--search-input' placeholder="Search for a deal" value={searchAddress} onChange={this.handleChangeInput('searchAddress')}/>
+	            </div>
+	            <div className="header--address">My Account</div>
+	          </div>
+	          <div className="header--bottom">
+	            <div className="balance-wrap">
+	              <span className="balance-name">BALANCE</span>
+	              {
+	                this.props.address && (<div className="header--address">
+	                  <button>My Account</button>
+	                  <button onClick={this.handleLogout}>Logout</button>
+	                </div>)
+	              }
+	            </div>
+	            <div className="header--bottom">
+	              <Balance balance={12}/>
+	              <div className="header--action">
+	                <div className="header--intro">
+	                  <div className="intro-title">DESCRO</div>
+	                  <div className="intro-body">Decentralized Escrow for Ethereum</div>
+	                </div>
+	                <button className="btn" onClick={this.handleModal(true)}>Start Deal</button>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+	      <Modal key='modal' isShow={isShow} hideModal={this.handleModal(false)}>
+	        Start Deal Modal
+	        <div>
+	          <label htmlFor="buyerAddress">Seller Address</label>
+	          <input type='text' id="buyerAddress" value={startAddress} onChange={this.handleChangeInput('startAddress')}/>
+	        </div>
+	        <div>
+	          <button className="btn" onClick={this.handleModal(false)}>Cancel</button>
+	          <button className="btn" onClick={this.createEscrow}>Confirm</button>
+	        </div>
+	      </Modal>
+	    </Fragment>
+		)
+  }
+}
 
-	export default Header
+export default Header
