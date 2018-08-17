10cfb95788f3cd8fb9f87524f40ff193369d4bd3
Junho Park <cnaa97@gmail.com>
2018-08-17 17:23:02 +0900

    pass props from App component

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -8,8 +8,8 @@ class App extends Component {
   render() {
     return (
       <div className="wrap">
-        <Header />
-        <Content />
+        <Header {...this.props} />
+        <Content {...this.props} />
       </div>
     );
   }
