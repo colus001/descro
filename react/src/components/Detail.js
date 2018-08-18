5664f28335a95205ed562f3aa62eebeff19d5e55
Junho Park <cnaa97@gmail.com>
2018-08-18 23:58:50 +0900

    apply contract actions

--- react/src/components/Detail.js
+++ react/src/components/Detail.js
@@ -1,45 +1,48 @@
 import React, { Component, Fragment } from 'react'
 
-import { formatDate, formatStatus } from '../utils/formatters'
-
+import { formatDate, formatStatus, STATUS } from '../utils/formatters'
 import { parseContract } from './EscrowList'
+import { etherToWei, getWeb3, weiToEther } from '../utils/ethereum'
+import Modal from './Modal'
+import Loading from './Loading'
 import history from '../history'
+
 import './Detail.css'
 
 const ActionTypes = [
   {
     name: 'deposit',
-    status: 1, // DEPOSITED
-    show: 'all',
+    status: STATUS.DEPOSITED,
+    show: 'buyer',
     payable: true,
   },
   {
     name: 'send product',
-    status: 1, // DEPOSITED
+    status: STATUS.DEPOSITED,
     show: 'seller',
     payable: false,
   },
   {
     name: 'approve',
-    status: 2, // PRODUCT_SENT
+    status: STATUS.PRODUCT_SENT,
     show: 'buyer',
     payable: false,
   },
   {
     name: 'cancel',
-    status: 2, // PRODUCT_SENT
+    status: STATUS.DEPOSITED,
     show: 'all',
     payable: false,
   },
   {
     name: 'withdraw',
-    status: 3, // APPROVED
+    status: STATUS.APPROVED,
     show: 'seller',
     payable: false,
   },
   {
     name: 'refund',
-    status: 3, // APPROVED
+    status: STATUS.CANCELLED,
     show: 'buyer',
     payable: false,
   },
@@ -48,120 +51,184 @@ const ActionTypes = [
 class Detail extends Component {
   state = {
     id: '',
-    escrow: {}
+    escrow: {},
+    showModal: false,
+    isLoading: false,
+    balanceValue: ''
   }
 
   componentDidMount() {
-    const { address, match } = this.props
+    const { match } = this.props
     
     if (!match.params.id) {
       history.push('/')
       return
     }
 
-    this.getDetailData(match.params.id)
-    this.getIdentity(address)
-  }
-
-  getDetailData(id) {
-    const { contract } = this.props
-
-    contract && contract.escrows && contract.escrows.call(id).then(escrow => {
-      this.setState({
-        id,
-        escrow: parseContract(escrow)
-      })
+    this.setState({
+      id: match.params.id
+    }, () => {
+      this.getDetailData()
     })
   }
 
-  getIdentity = address => {
-    // buyer address == current address -> buyer
-    // seller address == current address -> seller
-    // not match -> no body
+  getDetailData(updateBalance) {
+    const { contract } = this.props
+    const { id } = this.state
+
+    this.setState({ isLoading: true }, () => {
+      contract.escrows.call(id).then(escrow => {
+        this.setState({
+          escrow: parseContract(escrow),
+          ...updateBalance ? {} : {isLoading: false}
+        })
+      })
+
+      if (updateBalance) {
+        getWeb3()
+          .then((instance) => instance.eth.getBalance(this.props.address))
+          .then((balance) => this.props.setBalance(weiToEther(balance)))
+          .then(() => this.setState({ isLoading: false }))
+      }
+    })
   }
 
-  handleClick = action => () => {
-    // action에 따른 action
-
-    // const { contract } = this.props
-    // switch (action) {
-    //   case 'deposit':
-    //     this.props.contract
-    //       .createNewEscrow
-    //       .sendTransaction(startAddress, { from: this.props.address, value: etherToWei(buyerValue) })
-    //       .then((result) => {
-            
-    //       }, (err) => {
-    //         err && console.error(err)
-    //       })    
-    //     break
-    // }
+  handleContractAction = actionName => () => {
+    switch (actionName) {
+      case 'deposit':
+        this.handleModal(true)
+        this.handleModalConfirm = () => this.contractFunc('deposit', {value: etherToWei(this.state.balanceValue)})
+        break
+      case 'send product':
+        this.contractFunc('sendProduct')
+        break
+      case 'approve':
+      case 'cancel':
+      case 'refund':
+      case 'withdraw':
+        this.contractFunc(actionName)
+        break
+    }
   }
+
+  contractFunc(funcNs, opt) {
+    if (!funcNs) return
+
+    const { contract, address } = this.props
+    const { id } = this.state
+
+    contract
+      [funcNs]
+      .sendTransaction(id, { from: address, ...opt })
+      .then((result) => { // success
+        this.handleModal(false)
+        this.setState({ isLoading: true })
+        setTimeout(() => {
+          this.getDetailData(true)
+        }, 7000)
+      })
+      .catch(console.error)
+  }
+
+  handleModalConfirm () {}
+
+  handleModal = (isShow, callback) => {
+    this.setState({
+      showModal: isShow,
+      ...isShow ? {} : {balanceValue: ''}
+    }, () => {
+      callback && typeof callback === 'function' && callback()
+    })
+  }
+
+  handleChangeInput = (name) => (e) => this.setState({[name]: e.target.value})
 
   render() {
-    const { escrow, id } = this.state
+    const { escrow, id, showModal, balanceValue, isLoading } = this.state
     const { address } = this.props
     const isBuyer = escrow.buyer && new RegExp(address, 'i').test(escrow.buyer)
     const isSeller = escrow.seller && new RegExp(address, 'i').test(escrow.seller)
 
     return (
-      <div className="Detail container">
-        {escrow && (
-          <Fragment>
-            <h2>
-              Escrow Detail <small>{id}</small>
-            </h2>
-            <br />
-            <div className="Detail__content">
-              <table>
-                <thead>
-                  <tr>
-                    <th>Contract #</th>
-                    <th>Date</th>
-                    <th>buyer</th>
-                    <th>seller</th>
-                    <th>Status</th>
-                    <th>value</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>{id}</td>
-                    <td>{formatDate(escrow.createdAt)}</td>
-                    <td>{escrow.buyer}</td>
-                    <td>{escrow.seller}</td>
-                    <td>{formatStatus(escrow.status)}</td>
-                    <td>{escrow.balance} ether</td>
-                  </tr>
-                </tbody>
-              </table>
+      <Fragment>
+        {isLoading && <div className='modal-bg'><Loading /></div>}
+        <Modal 
+          isShow={showModal}
+          showBtns={true} 
+          hideModal={() => this.handleModal(false)} 
+          confirmFunc={this.handleModalConfirm}
+          cancelFunc={() => this.handleModal(false)}
+        >
+          <h3>Set a balance</h3>
+          <label htmlFor="balanceValue">
+            Balance
+          </label>
+          <input
+            type="text"
+            id="balanceValue"
+            placeholder="Enter balance"
+            value={balanceValue}
+            onChange={this.handleChangeInput('balanceValue')}
+          />
+        </Modal>
+        <div className="Detail container">
+          {escrow && (
+            <Fragment>
+              <h2>
+                Escrow Detail <small>{id}</small>
+                <div>
+                  <i className="fas fa-sync Balance__sync" onClick={() => this.getDetailData()} />
+                </div>
+                <br/>
+                {isBuyer && <div>BUYER <i className="fas fa-wallet" /></div>}
+                {isSeller && <div>SELLER<i className="fas fa-suitcase-rolling" /></div>}
+              </h2>
+              <br />
+              <div className="Detail__content">
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Contract #</th>
+                      <th>Date</th>
+                      <th>buyer</th>
+                      <th>seller</th>
+                      <th>Status</th>
+                      <th>value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{id}</td>
+                      <td>{formatDate(escrow.createdAt)}</td>
+                      <td>{escrow.buyer}</td>
+                      <td>{escrow.seller}</td>
+                      <td>{formatStatus(escrow.status)}</td>
+                      <td>{escrow.balance} ether</td>
+                    </tr>
+                  </tbody>
+                </table>
 
-              {
-                ActionTypes.map(action => {
-                  let show = true
-                  switch (action.show) {
-                    case 'seller':
-                      show = isSeller
-                      break;
-                    case 'buyer':
-                      show = isBuyer
-                      break;
-                  }
-
-                  return action.status === escrow.status && show ? (
-                    <button
-                      key={action.name}
-                      className="button button-outline"
-                      onClick={this.handleClick(action)}
-                    >
-                      {action.name}
-                    </button>
-                  ) : ''})
-              }
-            </div>
-          </Fragment>
-        )}
-      </div>
+                {
+                  ActionTypes.map(action => {
+                    let show = true
+                    action.show === 'seller' && (show = isSeller)
+                    action.show === 'buyer' && (show = isBuyer)
+                    
+                    return (action.status === escrow.status) && show ? (
+                      <button
+                        key={action.name}
+                        className="button button-outline"
+                        onClick={this.handleContractAction(action.name)}
+                      >
+                        {action.name}
+                      </button>
+                    ) : ''})
+                }
+              </div>
+            </Fragment>
+          )}
+        </div>
+      </Fragment>
     )
   }
 }
--- react/src/containers/DetailContainer.js
+++ react/src/containers/DetailContainer.js
@@ -2,13 +2,17 @@ import { connect } from 'react-redux'
 
 import Detail from '../components/Detail'
 
+import { setBalance } from '../state/actions/wallet'
+
 const mapStateToProps = state => ({
   contract: state.contract,
   address: state.wallet.address,
   balance: state.wallet.balance,
 })
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => ({
+  setBalance: (balance) => dispatch(setBalance(balance)),
+})
 
 export default connect(
   mapStateToProps,
